abb217109c090ce7c0673b50a58a818b48cd1e06
iiandmy <frezhgrand@gmail.com>
2024-04-22 03:47:54 +0300

    refactoring: remove useTabs rule, remove boilerplate api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,18 @@ import Head from 'next/head'
 import { Fragment } from 'react'
 
 export default function Home() {
-	return (
-		<Fragment>
-			<Head>
-				<title>Plaify</title>
-				<meta
-					name="description"
-					content="Plaify is a modern web music player"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main>Hello, World!</main>
-		</Fragment>
-	)
+  return (
+    <Fragment>
+      <Head>
+        <title>Plaify</title>
+        <meta
+          name="description"
+          content="Plaify is a modern web music player"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Hello, World!</main>
+    </Fragment>
+  )
 }
